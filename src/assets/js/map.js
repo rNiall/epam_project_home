9eb8dc8b162f111531764758b20ab218c0d1b91f
Naill <2jetsetter@gmail.com>
2017-06-08 19:42:21 +0300

    Add all markers on google map

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -109,7 +109,7 @@ function initMap() {
     var locations = [
         {
             title: 'test',
-            position: {lat: 50.9342802, lng: 30.3350986},
+            position: {lat: 62.6349438, lng: 35.9600986},
             icon: {
                 url: "assets/img/marker.png",
                 scaledSize: new google.maps.Size(18, 27)
@@ -118,7 +118,45 @@ function initMap() {
         },
         {
             title: 'test',
-            position: {lat: 48.85484, lng: 2.366427},
+            position: {lat: 55.82740807, lng: 26.1163486},
+            icon: {
+                url: "assets/img/marker.png",
+                scaledSize: new google.maps.Size(18, 27)
+            }
+        },
+        {
+            title: 'test',
+            position: {lat: 52.53782677, lng: 19.4366611},
+            icon: {
+                url: "assets/img/marker.png",
+                scaledSize: new google.maps.Size(18, 27)
+            }
+        },{
+            title: 'test',
+            position: {lat: 46.3790165, lng: 33.1475986},
+            icon: {
+                url: "assets/img/marker.png",
+                scaledSize: new google.maps.Size(18, 27)
+            }
+        },{
+            title: 'test',
+            position: {lat: 42.36043147, lng: 126.6632236},
+            icon: {
+                url: "assets/img/marker.png",
+                scaledSize: new google.maps.Size(18, 27)
+            }
+        },
+        {
+            title: 'test',
+            position: {lat: 34.96008882, lng: -84.9774014},
+            icon: {
+                url: "assets/img/marker.png",
+                scaledSize: new google.maps.Size(18, 27)
+            }
+        },
+        {
+            title: 'test',
+            position: {lat: 44.90438729, lng: -107.1258389},
             icon: {
                 url: "assets/img/marker.png",
                 scaledSize: new google.maps.Size(18, 27)
@@ -131,7 +169,7 @@ function initMap() {
             position: element.position,
             map: map,
             title: element.title,
-            icon: element.icon,
+            icon: element.icon
         });
     });
 }
