a3a73c31f6b01ef40baf4b023fee516ca441dcd3
Naill <2jetsetter@gmail.com>
2017-06-12 17:49:46 +0300

    Sort table

--- src/assets/js/imgMap.js
+++ src/assets/js/imgMap.js
@@ -3,6 +3,9 @@
  */
 
 var marker = document.querySelectorAll('.marker');
+var popup = document.querySelectorAll('.marker .popuptext');
+console.log(marker);
+console.log(popup);
 for(var i=0 ; i < marker.length ; i++){
     marker[i].addEventListener('click', function(){
         this.querySelector('.popuptext').classList.toggle('show');
