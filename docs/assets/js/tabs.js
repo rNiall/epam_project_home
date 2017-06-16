c7ca1548bd28bd3e062653f90d93c20c047f4203
Naill <2jetsetter@gmail.com>
2017-06-16 09:35:28 +0300

    Refactoring tabs

--- docs/assets/js/tabs.js
+++ docs/assets/js/tabs.js
@@ -1,49 +1,37 @@
-/*function openTab(evt, tabName) {
-    var i, tabContent, tabLinks;
-    tabContent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
-    }
-    tabLinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tabLinks.length; i++) {
-        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-
-}
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-    */
-
 'use strict';
 
-function Tabs() {
+window.onload = function Tabs() {
     var bindAll = function() {
         var menuElements = document.querySelectorAll('[data-tab]');
         for(var i = 0; i < menuElements.length ; i++) {
-            menuElements[i].addEventListener('click', change, false);
+            menuElements[i].addEventListener('click', change);
         }
-    }
+    };
 
     var clear = function() {
         var menuElements = document.querySelectorAll('[data-tab]');
         for(var i = 0; i < menuElements.length ; i++) {
             menuElements[i].classList.remove('active');
-            var id = menuElements[i].getAttribute('data-tab');
-            document.getElementById(id).classList.remove('active');
+           var id = document.querySelectorAll('.tabcontent');
+           id[i].classList.remove('active');
         }
-    }
+    };
 
     var change = function(e) {
         clear();
         e.target.classList.add('active');
         var id = e.currentTarget.getAttribute('data-tab');
-        document.getElementById(id).classList.add('active');
-    }
+       // document.getElementById(id).classList.add('active');
+        if (document.getElementById(id) === null){
+            var tab = document.querySelectorAll('.tabcontent');
+            tab[0].classList.add('active');
+
+        } else {
+            document.getElementById(id).classList.add('active');
+        }
+    };
 
     bindAll();
-}
+};
 
-var connectTabs = new Tabs();
+// Tabs();
