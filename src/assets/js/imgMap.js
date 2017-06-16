f8c0eb66c31be61d9e4e9b9dc09b6f76b941147d
Naill <2jetsetter@gmail.com>
2017-06-16 10:44:18 +0300

    Delete useless

--- src/assets/js/imgMap.js
+++ src/assets/js/imgMap.js
@@ -1,26 +1,4 @@
-/**
- * Created by Stanislav on 12.06.2017.
- */
-/*
-var marker = document.querySelectorAll('.marker');
-var popup = document.querySelectorAll('.popuptext');
-var myVar;
 
-for(var i=0 ; i < marker.length ; i++){
-    document.querySelector('.popuptext').remove('show');
-    marker[i].addEventListener('click', function(){
-        // if (popup[i].contains('show') === true){
-        //     popup[i].remove('show');
-        // }
-
-        this.querySelector('.popuptext').classList.toggle('show');
-
-        // this.querySelector('.popuptext').remove('show');
-        console.log(this.querySelector('.popuptext').classList);
-        console.log(this.getElementsByClassName('popuptext'));
-    });
-}
-*/
 var marker = document.querySelectorAll('.marker');
 for(var i=0 ; i < marker.length ; i++){
     marker[i].addEventListener('click', function(){
