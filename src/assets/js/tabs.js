a061767d93d1e996189d88063ef717677bfcb9ad
Naill <2jetsetter@gmail.com>
2017-06-16 07:14:54 +0300

    Refactoring

--- src/assets/js/tabs.js
+++ src/assets/js/tabs.js
@@ -1,4 +1,4 @@
-function openTab(evt, tabName) {
+/*function openTab(evt, tabName) {
     var i, tabContent, tabLinks;
     tabContent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabContent.length; i++) {
@@ -15,3 +15,35 @@ function openTab(evt, tabName) {
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+    */
+
+'use strict';
+
+function Tabs() {
+    var bindAll = function() {
+        var menuElements = document.querySelectorAll('[data-tab]');
+        for(var i = 0; i < menuElements.length ; i++) {
+            menuElements[i].addEventListener('click', change, false);
+        }
+    }
+
+    var clear = function() {
+        var menuElements = document.querySelectorAll('[data-tab]');
+        for(var i = 0; i < menuElements.length ; i++) {
+            menuElements[i].classList.remove('active');
+            var id = menuElements[i].getAttribute('data-tab');
+            document.getElementById(id).classList.remove('active');
+        }
+    }
+
+    var change = function(e) {
+        clear();
+        e.target.classList.add('active');
+        var id = e.currentTarget.getAttribute('data-tab');
+        document.getElementById(id).classList.add('active');
+    }
+
+    bindAll();
+}
+
+var connectTabs = new Tabs();
